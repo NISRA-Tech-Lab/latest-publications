09e23ce5ed23cb2c70487c8a2a5036da50a52145
brianq-nisra <brian.quinn@nisra.gov.uk>
2024-07-18 17:42:03 +0100

    sort upcoming on publication date

--- Scrape gov uk site for upcoming NISRA pubs.R
+++ Scrape gov uk site for upcoming NISRA pubs.R
@@ -1,10 +1,12 @@
 library(xml2)
 library(rvest)
+library(dplyr)
+library(lubridate)
 
-output_html <- c('<!DOCTYPE html>',
-                 '<html lang="en">',
-                 '<body>',
-                 '<ul>')
+
+pub_info <- data.frame(pub_title = character(),
+                       meta_data = character())
+
 
 for (i in 1:5) {
   
@@ -17,19 +19,52 @@ for (i in 1:5) {
   
   for (j in 1:length(publications)) {
     
-    pub_title <- paste0("<li>", html_text(html_nodes(publications[j], "title")))
+    pub_title <- html_text(html_nodes(publications[j], "title"))
     
     gov_uk_link <- html_attr(html_nodes(publications[j], "link"), "href")
     
     gov_uk_page <- read_html(gov_uk_link)
     
-    meta_data <- paste0("<div><strong>Release date:</strong> ",
-                        html_text(html_nodes(gov_uk_page, "dd"))[html_attr(html_nodes(gov_uk_page, "dd"), "class") == "app-c-important-metadata__definition"],
-                        "</div></li>")
+    meta_data <- html_text(html_nodes(gov_uk_page, "dd"))[html_attr(html_nodes(gov_uk_page, "dd"), "class") == "app-c-important-metadata__definition"]
     
-    output_html <- c(output_html, pub_title, meta_data)
+    pub_info <- pub_info %>%
+      rbind(data.frame(pub_title = pub_title,
+                       meta_data = meta_data))
     
   }
+  
+}
+
+pub_info <- pub_info %>%
+  mutate(release_day = as.numeric(substr(meta_data, 1, 2)),
+         release_month = case_when(is.na(release_day) ~ sub(" .*", "", meta_data),
+                                   TRUE ~ sub("^\\s*(\\S+\\s+\\S+).*", "\\1", meta_data) %>%
+                                     sub(".* ", "", .)),
+         release_year = case_when(grepl(year(today()), meta_data) ~ year(today()),
+                                  grepl(year(today()) + 1, meta_data) ~ year(today()) + 1,
+                                  grepl(year(today()) + 2, meta_data) ~ year(today()) + 2),
+         release_month_numeric = match(release_month, month.name),
+         release_day_fixed = case_when(is.na(release_day) & release_month_numeric %in% c(1, 3, 5, 7, 8, 10, 12) ~ 31,
+                                       is.na(release_day) & release_month_numeric %in% c(4, 6, 9, 11) ~ 30,
+                                       is.na(release_day) & release_month_numeric == 2 ~ 28,
+                                       TRUE ~ release_day),
+         release_date = as.Date(paste(release_year, release_month_numeric, release_day_fixed, sep = "-")),
+         meta_data = case_when(release_date < today() ~ paste(meta_data, "(delayed)"),
+                               TRUE ~ meta_data)) %>%
+  arrange(release_date)
+
+output_html <- c('<!DOCTYPE html>',
+                 '<html lang="en">',
+                 '<body>',
+                 '<ul>')
+
+for (i in 1:nrow(pub_info)) {
+  
+  output_html <- c(output_html,
+                   paste0("<li>", pub_info$pub_title[i],
+                    "<div><strong>Release date:</strong> ",
+                    pub_info$meta_data[i],
+                    "</div></li>"))
   
 }
 
