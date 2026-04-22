aab17c878ad0d446017e4ec970a825836c79cbfd
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-04-22 14:15:50 +0100

    update csv

--- upcoming_research_from_csv.R
+++ upcoming_research_from_csv.R
@@ -1,4 +1,7 @@
-upcoming_research <- read.csv("upcoming-research.csv")
+upcoming_research <- read.csv("upcoming-research.csv") %>% 
+  mutate(url = case_when(url == "" ~ NA,
+                         TRUE ~ url))
+
 
 for (i in 1:nrow(upcoming_research)) {
   
@@ -8,9 +11,11 @@ for (i in 1:nrow(upcoming_research)) {
     str_to_lower() %>%
     str_replace_all("[^a-z0-9 ]", "") %>%
     str_replace_all(" +", "-")
+  
+  new_output_index <- length(output_list$entries) + 1
 
   
-  output_list$entries[[length(output_list$entries) + 1]] <-
+  output_list$entries[[new_output_index]] <-
     list(id = id,
          title = entry$title,
          summary = entry$summary,
@@ -20,4 +25,9 @@ for (i in 1:nrow(upcoming_research)) {
          org = entry$org,
          type = "R",
          status = entry$status)
+  
+  if (!is.na(entry$url)) {
+    output_list$entries[[new_output_index]]$url <- entry$url
+  }
+  
 }
